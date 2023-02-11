120d29823372638b0080d51b72cc9750a1c6163d
Matthew Noel <matthew.w.noel@gmail.com>
2023-02-10 22:19:05 -0600

    Rendering scratchblocks in svelte proof of concept

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,25 @@
+<script>
+    import scratchblocks from 'scratchblocks';
+    const script = `
+        when green flag clicked
+        move (4) steps
+    `;
+    const doc = scratchblocks.parse(script, {
+        languages: ['en'],
+    });
+    const view = scratchblocks.newView(doc, {
+        style: 'scratch3',
+        scale: 0.675,
+    });
+    const svg = view.render();
+</script>
+
 <h1>App</h1>
+<h2>Guide</h2>
+{@html svg.outerHTML}
+
+<style>
+    h2 {
+        color: red;
+    }
+</style>
