e81e571bb22b04902684e7572d6c27df4e82ac39
Matthew Noel <matthew.w.noel@gmail.com>
2023-02-12 10:36:17 -0600

    Help links and project placeholders

--- src/components/ProjectsList.svelte
+++ src/components/ProjectsList.svelte
@@ -2,10 +2,27 @@
     import { updatePage } from "../router";
 </script>
 
+<h1>All Projects</h1>
 <div class="l0">
+    <h3>Getting Started</h3>
     <ul>
         <li>
             <span on:click={() => updatePage('project')} on:keydown={() => updatePage('project')}>Hello World 👋</span>
         </li>
     </ul>
+    <h3>Classic Games</h3>
+    <ul>
+        <li>
+            <span on:click={() => updatePage('project')} on:keydown={() => updatePage('project')}>Pong 🏓</span>
+        </li>
+        <li>
+            <span on:click={() => updatePage('project')} on:keydown={() => updatePage('project')}>Tic-Tac-Toe #️⃣</span>
+        </li>
+    </ul>
 </div>
+
+<style>
+    h1 {
+        text-decoration: underline;
+    }
+</style>
--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,4 +1,9 @@
+<script>
+    import GitLinks from "../components/GitLinks.svelte";
+</script>
+
 <div class="l0">
     <h1>About</h1>
     <p>This site contains step-by-step guides for building classic, simple games in Scratch 3.0. It is open source and available on <a target="_blank" rel="noreferrer" href="https://github.com/matthewnoel/scratch-lesson-plans">github</a>.</p>
 </div>
+<GitLinks page={'blob/main/src/pages/About.svelte'} />
